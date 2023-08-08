6ca431a41d64b95fe296c3906f937be7073c518d
lokeshk-incresco <lokesh.kasi@increscotech.com>
2023-08-08 12:34:29 +0530

    fix:form group

--- src/login/LoginPage.jsx
+++ src/login/LoginPage.jsx
@@ -253,7 +253,7 @@ class LoginPage extends React.Component {
           {activationMsgType && <AccountActivationMessage messageType={activationMsgType} />}
           {this.props.resetPassword && !this.props.loginError ? <ResetPasswordSuccess /> : null}
           <Form name="sign-in-form" id="sign-in-form">
-            <FormGroup
+            {/* <FormGroup
               name="emailOrUsername"
               value={this.state.emailOrUsername}
               autoComplete="on"
@@ -296,7 +296,7 @@ class LoginPage extends React.Component {
               onClick={this.handleForgotPasswordLinkClickEvent}
             >
               {intl.formatMessage(messages['forgot.password'])}
-            </Link>
+            </Link> */}
             {this.renderThirdPartyAuth(providers, secondaryProviders, currentProvider, thirdPartyAuthApiStatus, intl)}
           </Form>
         </div>
